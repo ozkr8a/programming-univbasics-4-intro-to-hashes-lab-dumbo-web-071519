81930fac73c4aeac9277eb56d6f21009779a7602
Oscar <oscar_ochoac@yahoo.com>
2019-06-30 04:08:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   
   if hash[key]
-    return hash[key] += 1
+    hash[key] += 1
   else
     hash[key] = 1
   end
